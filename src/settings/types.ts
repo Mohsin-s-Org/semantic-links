3cb3312094d088824064885f0a7bece7f032c6e0
mohsinosman <mohsinosman.enquiries@gmail.com>
2026-08-05 15:21:25 +0100

    feat: define versioned settings schema

---
+++ src/settings/types.ts
@@ -0,0 +1,22 @@
+import { SETTINGS_VERSION } from "../constants.ts";
+
+export type LinkPathMode = "shortest" | "full";
+
+export interface SemanticLinksSettings {
+  settingsVersion: typeof SETTINGS_VERSION;
+  automaticSuggestions: boolean;
+  lexicalMatchingEnabled: boolean;
+  semanticIndexingEnabled: boolean;
+  debounceMs: number;
+  maxSuggestions: number;
+  minimumConfidence: number;
+  excludedFolders: string[];
+  excludedTags: string[];
+  linkPathMode: LinkPathMode;
+}
+
+export interface SettingsLoadResult {
+  settings: SemanticLinksSettings;
+  migrated: boolean;
+  warnings: string[];
+}
